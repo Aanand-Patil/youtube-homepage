9820589893ca3abe9bcc9effc72dac0455437de0
Aanand-Patil <anandpatil6440@gmail.com>
2024-12-18 00:09:00 +0530

    category props udated, selected category dynamically updated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { CategoryPills } from "./components/CategoryPills";
 import { categories } from "./data/home";
 import { PageHeader } from "./layouts/PageHeader";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
   return (
     <div className="max-h-full flex flex-col ">
       <PageHeader />
@@ -10,7 +13,11 @@ function App() {
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto ">
         <div>Sidebar</div>
         <div className=" sticky top-0 bg-white z-10 pb-4">
-          <CategoryPills categories={categories} />
+          <CategoryPills
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onSelect={setSelectedCategory}
+          />
         </div>
       </div>
     </div>
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -2,16 +2,22 @@ import { Button } from "./Button";
 
 type CategoryPillsProps = {
   categories: string[];
+  selectedCategory: string;
+  onSelect: (category: string) => void;
 };
 
-export function CategoryPills({ categories }: CategoryPillsProps) {
+export function CategoryPills({
+  categories,
+  selectedCategory,
+  onSelect,
+}: CategoryPillsProps) {
   return (
     <div className="overflow-x-hidden relative">
       <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
         {categories.map((category) => (
           <Button
             key={category}
-            variant="dark"
+            variant={selectedCategory === category ? "dark" : "default"}
             className="py-1 px-3 rounded-lg whitespace-nowrap "
           >
             {category}
