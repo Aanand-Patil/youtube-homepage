358f0933a7775f50784cc6127c9f21fd81e7d987
Aanand-Patil <anandpatil6440@gmail.com>
2024-12-18 00:25:20 +0530

    ChevronLeft scrollbar button transfomred.

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeft } from "lucide-react";
 import { Button } from "./Button";
 
 type CategoryPillsProps = {
@@ -17,12 +18,22 @@ export function CategoryPills({
         {categories.map((category) => (
           <Button
             key={category}
+            onClick={() => onSelect(category)}
             variant={selectedCategory === category ? "dark" : "default"}
             className="py-1 px-3 rounded-lg whitespace-nowrap "
           >
             {category}
           </Button>
         ))}
+      </div>
+      <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-full aspect-square w-auto p-1.5"
+        >
+          <ChevronLeft />
+        </Button>
       </div>
     </div>
   );
